ceffeb19d45766f6c20a0330d169423b0ca7bbb3
Brady Hearn <bradythehuman@gmail.com>
2018-02-28 17:29:28 -0800

    commiting last nights work done in the study room on our floor

--- main.py
+++ main.py
@@ -61,16 +61,10 @@ class Game:
             self.screen = pygame.display.set_mode(self.resolution)
         pygame.font.init()
         # RUN LEVEL VARIABLES
-        # self.cursor_pos = [20, 20]
         self.my_ship = ship.Ship()
         self.crew = [crew.Crew()]
         self.mood = mood.Mood()
-        # self.selected_crew = False
-        # self.shortest_path_tree = {}
-        # self.path = []
-        # self.console = ["Doot Doot", "Deet Deet"]
-        # self.possible_actions = {}
-        self.states = [SelectAction(self)]
+        self.states = [SelectAction()]
         # LOAD GRAPHICS
         self.myfont = pygame.font.SysFont('Courier New', self.font_scale)
         self.bar_gui = pygame.image.load("bitmaps/bar_gui.bmp")
@@ -106,60 +100,9 @@ class Game:
                              (576, (i*self.font_scale)))
         pygame.display.flip()
 
-    # def render_frame(self):
-    #     self.screen.fill(self.black)
-    #     self.screen.blit(self.bar_gui, (0, 0))
-    #     self.screen.blit(self.my_ship.background, (0, 8 * self.scale))
-    #     for crew_member in self.crew:
-    #         pos = crew_member.pos
-    #         pygame.draw.rect(self.screen, self.crew_color, (pos[0] * 12 + 2, pos[1] * 12 + 98, 8, 8))
-    #     self.screen.blit(self.cursor, (self.cursor_pos[0] * 12, self.cursor_pos[1] * 12 + 96))
-    #     if self.path:
-    #         for i in range(1, len(self.path) - 1):
-    #             previous_pos = self.path[i - 1]
-    #             current_pos = self.path[i]
-    #             next_pos = self.path[i + 1]
-    #             previous_next_pos = (get_relative_direction(current_pos, previous_pos),
-    #                                  get_relative_direction(current_pos, next_pos))
-    #             self.screen.blit(self.pathsRed[DIRECTION_TO_PATH[previous_next_pos]], ship_to_screen_pos(current_pos))
-    #     for i in range(len(self.console)):
-    #         self.screen.blit(self.myfont.render(self.console[i], False, self.font_color), (576, (i*self.font_scale)))
-    #     pygame.display.flip()
-
     def update(self):
         for member in self.crew:
             member.update(self)
-
-    # def try_quit(self, event):
-    #     if event.key == pygame.K_ESCAPE:
-    #         sys.exit()
-    #
-    # def try_move_cursor(self, event):
-    #     if event.key == pygame.K_UP and self.cursor_pos[1] > 0:
-    #         self.cursor_pos[1] -= 1
-    #     elif event.key == pygame.K_DOWN and self.cursor_pos[1] < 48:
-    #         self.cursor_pos[1] += 1
-    #     elif event.key == pygame.K_LEFT and self.cursor_pos[0] > 0:
-    #         self.cursor_pos[0] -= 1
-    #     elif event.key == pygame.K_RIGHT and self.cursor_pos[0] < 48:
-    #         self.cursor_pos[0] += 1
-    #
-    # def try_select_crew(self, event):
-    #     if event.key == pygame.K_RETURN and self.cursor_pos in [x.pos for x in self.crew] and not self.selected_crew:
-    #         for x in self.crew:
-    #             if self.cursor_pos == x.pos:
-    #                 self.selected_crew = x
-    #                 self.shortest_path_tree = self.selected_crew.pathing(self.my_ship.map["floor"])
-    #     elif event.key == pygame.K_RETURN and self.selected_crew and self.path:
-    #         self.selected_crew.pos = list(self.cursor_pos)
-    #         self.selected_crew = False
-    #         self.shortest_path_tree = {}
-    #
-    # def update_path(self):
-    #     if tuple(self.cursor_pos) in self.shortest_path_tree:
-    #         self.path = self.shortest_path_tree[tuple(self.cursor_pos)]
-    #     else:
-    #         self.path = []
 
     def is_empty_floor(self, pos):
         if pos not in self.my_ship.map["floor"]:
@@ -169,29 +112,12 @@ class Game:
                 return False
         return True
 
-    # def select_action(self):
-    #     while True:
-    #         self.main_loop(keypress_funcs=[self.try_quit, self.try_move_cursor, self.check_actions])
-    #
-    # def check_actions(self, event):
-    #     if event == pygame.K_m:
-    #         self.move_unit()
-    #     elif event == pygame.K_s:
-    #         self.spawn_unit()
-    #     elif event == pygame.K_k:
-    #         self.kill_unit()
-    #
-    # def select_unit(self):
-    #     self.console = ["Select a crew member..."]
-    #     self.main_loop(keypress_funcs=[self.try_quit, self.try_move_cursor, self.try_select_crew])
-
 
 class State:
     console = ["Warning. This is the default state.", "Warning. This is the default state."]
 
-    def __init__(self, game):
+    def __init__(self):
         self.cursor_pos = [20, 20]
-        self.game = game
         self.path = []
 
     def try_quit(self, event):
@@ -215,40 +141,38 @@ class SelectAction(State):
     def check_event(self, event):
         self.try_quit(event)
         self.try_move_cursor(event)
-        if event == pygame.K_m:
-            self.game.states.append(SelectUnit(self.game, self.cursor_pos))
-        elif event == pygame.K_s:
+        if event.key == pygame.K_m:
+            game.states.append(SelectUnit(self.cursor_pos))
+        elif event.key == pygame.K_s:
             pass
-        elif event == pygame.K_k:
+        elif event.key == pygame.K_k:
             pass
 
 
 class SelectUnit(State):
     console = ["Select unit..."]
 
-    def __init__(self, game, cursor_pos):
+    def __init__(self, cursor_pos):
         self.cursor_pos = cursor_pos
-        self.game = game
         self.path = []
 
     def check_event(self, event):
         # Event Processing
         self.try_quit(event)
         self.try_move_cursor(event)
-        if event.key == pygame.K_RETURN and self.cursor_pos in [x.pos for x in self.game.crew]:
-            for x in self.game.crew:
+        if event.key == pygame.K_RETURN and self.cursor_pos in [x.pos for x in game.crew]:
+            for x in game.crew:
                 if self.cursor_pos == x.pos:
-                    self.game.states.append(SelectSpace(self.game, x))
+                    game.states.append(SelectSpace(x))
 
 
 class SelectSpace(State):
     console = ["Select space..."]
 
-    def __init__(self, game, selected_crew):
-        self.cursor_pos = selected_crew.pos
-        self.game = game
+    def __init__(self, selected_crew):
+        self.cursor_pos = list(selected_crew.pos)
         self.selected_crew = selected_crew
-        self.shortest_path_tree = selected_crew.pathing(self.game.my_ship.map["floor"])
+        self.shortest_path_tree = selected_crew.pathing(game.my_ship.map["floor"])
         self.path = []
 
     def check_event(self, event):
@@ -257,7 +181,8 @@ class SelectSpace(State):
         self.try_move_cursor(event)
         if event.key == pygame.K_RETURN and self.path:
             self.selected_crew.pos = list(self.cursor_pos)
-            self.game.states.pop()
+            game.states[-3].cursor_pos = list(self.cursor_pos)
+            game.states = game.states[:-2]
         # Update
         if tuple(self.cursor_pos) in self.shortest_path_tree:
             self.path = self.shortest_path_tree[tuple(self.cursor_pos)]
@@ -271,8 +196,9 @@ class Encounter:
 
 
 if __name__ == "__main__":
-    my_game = Game()
-    my_game.update()
+    # game is being used as a global variable because there is only ever one instance of Game() and it is constantly mutated
+    game = Game()
+    game.update()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -280,5 +206,5 @@ if __name__ == "__main__":
             elif event.type == pygame.KEYDOWN:
                 if event == pygame.K_m:
                     print("That's an m alright")
-                my_game.states[-1].check_event(event)
-        my_game.render_frame()
+                game.states[-1].check_event(event)
+        game.render_frame()
